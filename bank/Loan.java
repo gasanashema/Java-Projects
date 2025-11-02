12ca9737dd9ae12b774ce001643ceeaf8bd9431b
Shema <shemaphilbert8@gmail.com>
2025-11-02 14:28:56 +0200

    created a simple withdraw application

--- bank/Loan.java
+++ bank/Loan.java
@@ -1,13 +1,37 @@
 package bank;
 
 public class Loan {
-    int loanId;
-    double requestedAmount;
-    double approvedAmount;
+    private int loanId;
+    private double requestedAmount;
+    private double approvedAmount;
     
     public Loan(int loanId, double requestedAmount, double approvedAmount) {
         this.loanId = 0;
         this.requestedAmount = 0;
         this.approvedAmount = 0;
+    }
+
+    public int getLoanId() {
+        return loanId;
+    }
+
+    public void setLoanId(int loanId) {
+        this.loanId = loanId;
+    }
+
+    public double getRequestedAmount() {
+        return requestedAmount;
+    }
+
+    public void setRequestedAmount(double requestedAmount) {
+        this.requestedAmount = requestedAmount;
+    }
+
+    public double getApprovedAmount() {
+        return approvedAmount;
+    }
+
+    public void setApprovedAmount(double approvedAmount) {
+        this.approvedAmount = approvedAmount;
     }
 }
