d60d1020ed02245507ce4a2183e9ba10e95a0ca2
Shema <shemaphilbert8@gmail.com>
2025-11-09 15:38:40 +0200

    added validation to a student

--- auca/Faculty.java
+++ auca/Faculty.java
@@ -16,10 +16,17 @@ public class Faculty extends GishushuCampus {
     }
 
     public void setFacultyId(int facultyId) {
+        if (facultyId!=1) {
+            System.out.println("invalid Id");
+            return;
+        }
         this.facultyId = facultyId;
     }
 
     public void setFacultyName(String facultyName) {
+        if (!facultyName.equals("IM") || !facultyName.equals("SE") || !facultyName.equals("NT")) {
+            System.out.println("Invalid faculty. it must be IM, SE or NT");
+        }
         this.facultyName = facultyName;
     }
 
