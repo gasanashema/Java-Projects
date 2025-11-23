1be380081c42711e4cc3fca7e7635f0bdaf750e8
Shema <shemaphilbert8@gmail.com>
2025-11-23 13:08:34 +0200

    interfaces in java

---
+++ interfaces/DoctorImplementaion.java
@@ -0,0 +1,21 @@
+public class DoctorImplementaion implements Doctor {
+
+    @Override
+    public void addDoctor() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void deleteDoctor() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void modifyDoctor() {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
