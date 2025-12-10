a28fc597cbe063acb44876dc506b0b6f8802c8c9
Shema Philbert <shemaphilbert8@gmail.com>
2025-12-10 13:32:14 +0200

    feat: implemented vehicle system

--- vehicle-registration-and-licencing/src/VehicleSystem.java
+++ vehicle-registration-and-licencing/src/VehicleSystem.java
@@ -1,75 +1,84 @@
-public class VehicleSystem implements EnforcementOperations,RegistrationOpertations {
+import java.util.ArrayList;
 
-    @Override
-    public void blacklistVehicle() {
-        // TODO Auto-generated method stub
-        
-    }
+public class VehicleSystem implements RegistrationOpertations, EnforcementOperations {
 
-    @Override
-    public void calculatePenalty() {
-        // TODO Auto-generated method stub
-        
-    }
-
-    @Override
-    public void generateEnforcementReport() {
-        // TODO Auto-generated method stub
-        
-    }
-
-    @Override
-    public void recordViolation() {
-        // TODO Auto-generated method stub
-        
-    }
-
-    @Override
-    public void suspendRegistration() {
-        // TODO Auto-generated method stub
-        
-    }
-
-    @Override
-    public void verifyDocuments() {
-        // TODO Auto-generated method stub
-        
-    }
+    private ArrayList<Vehicle> vehicles = new ArrayList<>();
+    private ArrayList<Owner> owners = new ArrayList<>();
+    private ArrayList<Insurance> insurances = new ArrayList<>();
+    private ArrayList<RoadTax> roadTaxes = new ArrayList<>();
+    private ArrayList<Violation> violations = new ArrayList<>();
+    private ArrayList<Penalty> penalties = new ArrayList<>();
 
     @Override
     public void generateReport() {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Override
     public void registerVehicle() {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Override
     public void renewInsurance() {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Override
     public void renewRoadTax() {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Override
     public void searchVehicle() {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Override
     public void transferOwnership() {
         // TODO Auto-generated method stub
-        
+
     }
-    
+
+    @Override
+    public void blacklistVehicle() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void calculatePenalty() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void generateEnforcementReport() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void recordViolation() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void suspendRegistration() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void verifyDocuments() {
+        // TODO Auto-generated method stub
+
+    }
+
 }
